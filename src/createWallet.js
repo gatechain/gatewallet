79d680142e44454f5d333b4135a7ee8dec48f58a
Thone Liu <thorne_lew@126.com>
2022-09-27 09:59:22 +0800

    upgrade version 1.0.24

--- src/createWallet.js
+++ src/createWallet.js
@@ -214,7 +214,7 @@ function buildOrderCompressedData(tx, config) {
   let right = contractRightMap[contract_right]
   res = Scalar.add(res, Scalar.shl(right || 0, 56))
 
-  let price = ((new Deciamls(tx.price)).mul(Math.pow(10, 18))).toString();
+  let price = ((new Deciamls(tx.price)).mul(Math.pow(10, 18))).toFixed();
   res = Scalar.add(res, Scalar.shl(price || 0, 64));
 
   res = Scalar.add(res, Scalar.shl(Math.abs(tx.size) || 0, 192))
@@ -236,7 +236,7 @@ function buildWithdrawCompressedData(tx) {
   let res = Scalar.e(0)
 
   res = Scalar.add(res, tx.user_id || 0)
-  const amount = ((new Deciamls(tx.amount)).mul(Math.pow(10, 18))).toString();
+  const amount = ((new Deciamls(tx.amount)).mul(Math.pow(10, 18))).toFixed();
   res = Scalar.add(res, Scalar.shl(amount || 0, 48))
   return res
 }
